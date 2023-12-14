4849b4d63b7b7df91849ee464ac482cd4bc5bcab
alexxkrava <Kravchenkoalex94@gmail.com>
2023-12-15 00:46:33 +0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import datetime
+import os
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse("time"),
+        'Показать содержимое рабочей директории': reverse("workdir")
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    res = []
+    current_directory = os.getcwd()
+    files = os.listdir(current_directory)
+    for i in files:
+        res.append(i)
+        res.append(', ')
+    return HttpResponse(res)
+
